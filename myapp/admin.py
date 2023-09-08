ed78e0bebfc67c4d37e8f31c653e8f46ed997051
Ab-Romia <aabouroumia@gmail.com>
2023-09-08 10:35:30 +0300

    during models foreign course in coursera

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Menu, MenuCategory, ElMenu
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(MenuCategory)
+admin.site.register(ElMenu)
--- myapp/models.py
+++ myapp/models.py
@@ -9,6 +9,24 @@ class Menu(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     offer = models.BooleanField(default=False)
+    vibeCheck = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name + " - " + str(self.price) + " - " + self.description + " - " + str(self.offer)
+        return self.name + " - " + str(self.price) + " - " + self.description + " - " + str(self.offer) + " - " + str(
+            self.vibeCheck)
+
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ElMenu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
+
+    def __str__(self):
+        return self.name + " - " + str(self.price) + " - " + str(self.category)
