ce8ffcd21ab455891a669734c0a107ab7dd69093
Ab-Romia <aabouroumia@gmail.com>
2023-09-17 06:50:35 +0300

    before debuggin and testing

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('reservation/', views.ReservationView, name='reservation'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import forms
-from .models import Reservation
+from .models import Reservation, Person
 
 
 # Create a reservation view to take the reservation details from the user (name, contact, datetime, count, notes) and save it to the database.
@@ -23,3 +23,15 @@ def ReservationView(request):
         person_form = forms.PersonForm()
         reservation_form = forms.ReservationForm()
     return render(request, 'reservation.html', {'person_form': person_form, 'reservation_form': reservation_form})
+
+
+def home(request):
+    return render(request, "home.html", {})
+
+
+def register(request):
+    return render(request, "register.html", {})
+
+
+def login(request):
+    return render(request, "login.html", {})
